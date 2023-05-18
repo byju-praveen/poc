cf7d143921df2363228ffa8cc9efef962df5fd44
Praveen Prakash <121926334+byju-praveen@users.noreply.github.com>
2023-05-18 15:07:38 +0530

    First Commit

---
+++ lib/main.dart
@@ -0,0 +1,148 @@
+import 'dart:io';
+import 'dart:isolate';
+import 'dart:ui';
+import 'package:flutter/material.dart';
+import 'package:flutter_downloader/flutter_downloader.dart';
+import 'package:path_provider/path_provider.dart';
+import 'package:permission_handler/permission_handler.dart';
+import 'package:archive/archive.dart';
+
+void main() async {
+  WidgetsFlutterBinding.ensureInitialized();
+
+  // Plugin must be initialized before using
+  await FlutterDownloader.initialize(
+    debug: true,
+    ignoreSsl: true,
+  );
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Flutter Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: const MyHomePage(title: 'FlutterDownloader'),
+    );
+  }
+}
+
+class MyHomePage extends StatefulWidget {
+  const MyHomePage({Key? key, required this.title}) : super(key: key);
+
+  final String title;
+
+  @override
+  _MyHomePageState createState() => _MyHomePageState();
+}
+
+class _MyHomePageState extends State<MyHomePage> {
+  int progress = 0;
+
+  void _downloadFiles() async {
+    final status = await Permission.storage.request();
+    if (status.isGranted) {
+      final baseStorage = await getExternalStorageDirectory();
+      print(baseStorage.toString());
+      final savedDir = baseStorage!.path;
+      const fileName = 'filename.zip';
+      final id = await FlutterDownloader.enqueue(
+        url:
+            'https://s3-ap-southeast-1.amazonaws.com/tnl-public/interactions/sanitized_assets/staging/1577.zip?1626696053',
+        savedDir: savedDir,
+        fileName: fileName,
+      );
+      print(baseStorage.path.toString());
+    } else {
+      print('Permission is not granted');
+    }
+  }
+
+  ReceivePort receivePort = ReceivePort();
+
+  @override
+  void initState() {
+    IsolateNameServer.registerPortWithName(
+        receivePort.sendPort, 'downloadingId');
+    receivePort.listen((message) {
+      setState(() {
+        progress = message;
+      });
+    });
+    FlutterDownloader.registerCallback(downloadCallback);
+    super.initState();
+  }
+
+  static downloadCallback(id, status, progress) async {
+    SendPort? sendPort = IsolateNameServer.lookupPortByName('downloadingId');
+    sendPort!.send(progress);
+
+    if (status == DownloadTaskStatus.complete) {
+      final savedDir = (await getExternalStorageDirectory())!.path;
+      final zipFilePath = '$savedDir/filename.zip';
+      final unzipDirectory = Directory('$savedDir/unzipfile');
+
+      try {
+        final bytes = File(zipFilePath).readAsBytesSync();
+        final archive = ZipDecoder().decodeBytes(bytes);
+
+        for (final file in archive) {
+          final fileName = file.name;
+          if (file.isFile) {
+            final filePath = '$unzipDirectory/$fileName';
+            final outputFile = File(filePath);
+            outputFile.createSync(recursive: true);
+            outputFile.writeAsBytesSync(file.content as List<int>);
+          } else {
+            final directoryPath = '$unzipDirectory/$fileName';
+            Directory(directoryPath).create(recursive: true);
+          }
+        }
+        print('File unzipped successfully');
+
+        // Remove the zip file
+        File(zipFilePath).delete();
+
+        // Rename the unzipped directory to the desired name
+        final newDirectoryName = '$savedDir/unzipfile';
+        await unzipDirectory.rename(newDirectoryName);
+        print('Unzipped directory renamed');
+      } catch (e) {
+        print('Error during unzipping: $e');
+      }
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+      appBar: AppBar(
+        title: Text(widget.title),
+      ),
+      body: Center(
+        child: Column(
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            const Text(
+              'Download Percentage',
+            ),
+            Text(
+              '$progress',
+              style: Theme.of(context).textTheme.titleLarge,
+            ),
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        onPressed: _downloadFiles,
+        child: const Icon(Icons.add),
+      ),
+    );
+  }
+}
