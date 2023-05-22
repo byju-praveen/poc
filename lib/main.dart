9bbfc02cf1650c3370a196fb70ea8bc9d75915cd
Praveen Prakash <121926334+byju-praveen@users.noreply.github.com>
2023-05-22 09:58:36 +0530

    Unzip file

--- lib/main.dart
+++ lib/main.dart
@@ -43,18 +43,23 @@ class MyHomePage extends StatefulWidget {
 class _MyHomePageState extends State<MyHomePage> {
   int progress = 0;
   int folderCounter = 0; // New variable to keep track of folder count
+  int status = 9;
+
+  // ...
 
   void _downloadFiles() async {
     final baseDir = (await getTemporaryDirectory()).path;
-    final folderName = 'folder_${folderCounter++}'; // Increment folder count
+    print(baseDir);
+    const folderName = 'folder'; // Increment folder count
     final savedDir = '$baseDir/$folderName';
     const fileName = 'filename.zip';
     final filePath = '$savedDir/$fileName';
-
+    print(filePath);
     // Create the directory if it doesn't exist
     if (!Directory(savedDir).existsSync()) {
       Directory(savedDir).createSync(recursive: true);
     }
+    print("savedDir - $savedDir");
 
     // Check if the file already exists
     final file = File(filePath);
@@ -67,12 +72,15 @@ class _MyHomePageState extends State<MyHomePage> {
       url:
           'https://s3-ap-southeast-1.amazonaws.com/tnl-public/interactions/sanitized_assets/staging/1577.zip?1626696053',
       savedDir: savedDir,
+      openFileFromNotification: false,
       fileName: fileName,
     );
     print(savedDir);
-    print('Download task ID: $taskId'); // Added print statement
+    print('Download task ID: $taskId');
 
-    // Open the downloaded file
+    // Wait for the file to finish downloading
+
+    // ...
   }
 
   ReceivePort receivePort = ReceivePort();
@@ -93,16 +101,25 @@ class _MyHomePageState extends State<MyHomePage> {
   static downloadCallback(id, status, progress) async {
     SendPort? sendPort = IsolateNameServer.lookupPortByName('downloadingId');
     sendPort?.send(progress);
+    print('$status -----  ${DownloadTaskStatus.complete.value}');
+    if (status == DownloadTaskStatus.complete.value) {
+      print("1");
+      // final savedDir = (await getTemporaryDirectory()).path;
+      print("2");
+      const zipFilePath =
+          '/data/user/0/com.example.poc/cache/folder/filename.zip';
+      print("3");
+      const unzipDirectory =
+          '/data/user/0/com.example.poc/cache/folder/unzipfile';
 
-    if (status == DownloadTaskStatus.complete) {
-      final savedDir = (await getTemporaryDirectory()).path;
-      final zipFilePath = '$savedDir/filename.zip';
-      final unzipDirectory = Directory('$savedDir/unzipfile');
+      print("4");
 
       try {
+        print("5");
         final bytes = File(zipFilePath).readAsBytesSync();
+        print("6");
         final archive = ZipDecoder().decodeBytes(bytes);
-
+        print("7");
         for (final file in archive) {
           final fileName = file.name;
           if (file.isFile) {
@@ -114,17 +131,21 @@ class _MyHomePageState extends State<MyHomePage> {
             final directoryPath = '$unzipDirectory/$fileName';
             Directory(directoryPath).createSync(recursive: true);
           }
+          print("8");
         }
         print('File unzipped successfully');
-
+        print("9");
         // Remove the zip file
         File(zipFilePath).deleteSync();
-
+        print("10");
         // Rename the unzipped directory to the desired name
-        final newDirectoryName = '$savedDir/unzipfile';
-        await unzipDirectory.rename(newDirectoryName);
-        print('Unzipped directory renamed');
+        // const newDirectoryName =
+        //     '/data/user/0/com.example.poc/cache/folder/unzipfile';
+        // print("11");
+        // await unzipDirectory.rename(unzipDirectory);
+        // print('Unzipped directory renamed');
       } catch (e) {
+        print("12");
         print('Error during unzipping: $e');
       }
     }
